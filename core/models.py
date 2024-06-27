05dcc20dcb2d4b84b9146600efa32e5aeb4b9722
angel <an.perugini@duocuc.cl>
2024-06-27 00:00:17 -0400

    arreglos modelos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    producto_id = models.IntegerField(primary_key=True)
+    Precio=models.IntegerField()
+    descripcion=models.CharField( max_length=250)
+    cantidad= models.IntegerField()
+    estado_producto = models.BooleanField()
+    
+    
+class Accesorios(models.Model):
+    producto_id=models.ForeignKey(Producto,on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=100)
+    distribuidor=models.CharField(max_length=100)
+    
+class Vehiculo(models.Model):
+    producto_id=models.ForeignKey(Producto,on_delete=models.CASCADE)
+    marca=models.CharField(max_length=100)
+    modelo=models.CharField(max_length=100)
+
+
+
+class Tipo_usuario(models.Model):
+    id_tipo_usuario=models.IntegerField(Primary_kay=True)
+    nombre_tipo_usuario=models.CharField(max_length=50)
+    
+class Usuario(models.Model):
+    usuario_id=models.IntegerField(primary_key=True)   
+    nombre=models.CharField(max_length=50)
+    apellidop=models.CharField(max_length=50)
+    apellidom=models.CharField(max_length=50)
+    email=models.CharField(max_length=150)
+    nr_telefono=models.CharField(max_length=15)
+    contraseña= models.CharField(max_length=50)
+    estado_usuario=models.BooleanField()
+        
+class Venta(models.Model):
+    venta_id=models.IntegerField(primary_key=True)
+    fecha_venta=models.DateField()
+    total_venta=models.IntegerField()
+    cliente_run=models.models.ForeignKey( Usuario,on_delete=models.CASCADE)
+    
+
+class Venta_Producto(models.Model):
+    venta_id=models.ForeignKey(Producto,on_delete=models.CASCADE)
+    producto_id=models.ForeignKey(Venta,on_delete=models.CASCADE)
+    cantidad_producto=models.IntegerField()
+    subtotal_producto= models.IntegerField()
