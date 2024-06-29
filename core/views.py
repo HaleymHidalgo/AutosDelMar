73885e42f9239eae9da14fc8e8d614e07c5a77ba
HaleymHidalgo <haleymgabrielhidalgo@gmail.com>
2024-06-28 21:56:34 -0400

    Added more fields to pagProducto

--- core/views.py
+++ core/views.py
@@ -6,9 +6,7 @@ def home(request):
     if request.method == 'GET':
         #Aqui nosotros obtenemos todos los productos que estan en la base de datos
         vehiculos = models.Vehiculo.objects.all()
-        context = {
-            'vehiculos': vehiculos
-        }
+        context = {'vehiculos': vehiculos}
         #le vamos a pasarle el contexto a la plantilla
         return render(request, 'home.html', context)
 
